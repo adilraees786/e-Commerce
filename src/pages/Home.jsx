bd92d33c9ad74d2852d048d6457a4120e7f584ec
adilraees786 <adilraees911@gmail.com>
2024-10-26 12:49:28 +0500

    setcaterogy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,29 +6,29 @@ import HeroImage from '../assets/Images/hero-page.png'
 
 const Home = () => {
   return (
-    <div>
-      <div>
-        <div>
-          <div>
-            <div>SHOP BY CATEGORIES</div>
-            <ul>
-                {Caterogries.map((category, index) => (
-                  <li key={index}>
-                    
-                    {category}
-                  </li>
-                ))}
-              </ul>
-          </div>
+    <div className=' bg-white mt-2 px-4 md:px-16 lg:px-24'>
+      <div className=' container mx-auto py-4 flex flex-col md:flex-row space-x-2'>
+        <div className=' w-full md:w-3/12'>
+
+          <div className=' bg-red-600 text-white text-xs font-bold px-2 py-2.5'>SHOP BY CATEGORIES</div>
+          <ul>
+            {Caterogries.map((category, index) => (
+              <li key={index}>
+
+                {category}
+              </li>
+            ))}
+          </ul>
+
           <div>
             <img src={HeroImage} alt='' />
-<div>
-  <p> Code with Adil</p>
-  <h2>WELCOME TO E-SHOP</h2>
-  <p>MILLIONS + PRODUCTS</p>
-  <button> SHOP NOW</button>
+            <div>
+              <p> Code with Adil</p>
+              <h2>WELCOME TO E-SHOP</h2>
+              <p>MILLIONS + PRODUCTS</p>
+              <button> SHOP NOW</button>
 
-</div>
+            </div>
 
           </div>
         </div>

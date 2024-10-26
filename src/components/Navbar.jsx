b1b2cf5a661f1475fefdd7febde99844d8ca23a6
adilraees786 <adilraees911@gmail.com>
2024-10-26 11:35:33 +0500

    csss2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,13 @@ export default function Navbar() {
     return (
         <div className=' bg-white shadow-md'> 
             <div className=' container mx-auto px-4:px-16 lg:px-24 py-4 flex justify-between items-center'>
-                <div>
+                <div className=' text-lg flex-1 mx-4'>
                     <Link to="/">e-Shop</Link>
                 </div>
-                <div>
+                <div className=' relative flex-1 mx-4'>
                     <form>
                         <input type="text" placeholder=" Search Product" />
-                        <FaSearch></FaSearch>
+                        <FaSearch className=' absolute top-3 right-3 text-red-600'></FaSearch>
                     </form>
                 </div>
                 <div>

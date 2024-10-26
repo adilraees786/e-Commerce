ddf3ac5fc7b4d288f6138f08e919aeecea5985b4
adilraees786 <adilraees911@gmail.com>
2024-10-26 11:28:58 +0500

    icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,35 @@
 import React from 'react'
+import { FaSearch, FaShoppingCart, FaUser } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
-export default function Navbar(){
+
+export default function Navbar() {
     return (
         <div>
-         <div>
-             <div>
-                 <Link to="/">e-Shop</Link>
-             </div>
-             <div>
-                 <form>
-                     <input type="text" placeholder=" Search Product"  />
-                 </form>
-             </div>
-         </div>
+            <div>
+                <div>
+                    <Link to="/">e-Shop</Link>
+                </div>
+                <div>
+                    <form>
+                        <input type="text" placeholder=" Search Product" />
+                        <FaSearch></FaSearch>
+                    </form>
+                </div>
+                <div>
+                    <Link to="/cart">
+                        <FaShoppingCart />
+                    </Link>
+
+                    <button>
+                        Login | Register
+                    </button>
+                    <button>
+                        <FaUser />
+                    </button>
+
+                </div>
+            </div>
         </div>
-       )
+    )
 }

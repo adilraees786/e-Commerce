20a50626c2f3c6ffa95a579f13f22c38f1b69b9e
adilraees786 <adilraees911@gmail.com>
2024-10-26 11:32:45 +0500

    css1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
     return (
         <div className=' bg-white shadow-md'> 
-            <div>
+            <div className=' container mx-auto px-4:px-16 lg:px-24 py-4 flex justify-between items-center'>
                 <div>
                     <Link to="/">e-Shop</Link>
                 </div>

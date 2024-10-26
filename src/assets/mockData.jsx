31f20c1a634649d05c39283bdb52c05a5a1e9829
adilraees786 <adilraees911@gmail.com>
2024-10-26 19:33:14 +0500

    caterogyset

--- src/assets/mockData.jsx
+++ src/assets/mockData.jsx
@@ -1,3 +1,16 @@
+import image1 from "./Images/bag.jpg"
+import image2 from "./Images/sandle.jpg"
+import image3 from "./Images/watch.jpg"
+import image4 from "./Images/cap.jpg"
+import image5 from "./Images/boot1.jpg"
+import image6 from "./Images/dove.jpg"
+import image7 from "./Images/headphone.jpg"
+import image8 from "./Images/robot.jpg"
+import image9 from "./Images/wwatch.jpg"
+import image10 from "./Images/shirt.jpg" 
+
+
+
 export const Caterogries =[
     "Electronics",
     "Fashion",
@@ -6,3 +19,62 @@ export const Caterogries =[
     "Sports",
     "Automative",
 ];
+
+export const mocData =[
+    {
+        id:1,
+        image:image9,
+        Name:"product 1",
+       price: 29.9,
+
+    },
+
+    {
+        id:2,
+        image:image8,
+        Name:"product 2",
+       price: 49.9,
+
+    },
+
+    {
+        id:3,
+        image:image9,
+        Name:"product 1",
+       price: 59.9,
+
+    },
+    {
+        id:3,
+        image:image7,
+        Name:"product 3",
+      price: 39.9,
+
+    },
+    {
+        id:4,
+        image:image6,
+        Name:"product 4",
+       price: 49.9,
+
+    },
+    {
+        id:5,
+        image:image10,
+        Name:"product 5",
+       price: 69.99,
+
+    },
+    {
+        id:1,
+        image:image9,
+        Name:"product 1",
+        price:200,
+       price: 29.9,
+
+    },
+
+
+
+];
+

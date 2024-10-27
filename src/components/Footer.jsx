7620fb7f35004cfae6d8bb013f7ba336202347b5
adilraees786 <adilraees911@gmail.com>
2024-10-27 21:51:22 +0500

    addfooter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,28 +5,28 @@ import { FaFacebook, FaLinkedin, FaInstagram, FaGithub } from 'react-icons/fa';
 
 export const Footer = () => {
   return (
-    <footer>
-      <div>
+    <footer className='bg-gray-800 text-white py-8 px-4 md:px-16 lg:px-24'>
+      <div className=' container mx-auto grid grid-cols-1 md:grid-flow-col-3 gap-8'>
         <div>
-          <h3>e-Shop</h3>
-          <p>Your one-step fot all your needs. Shop with use and experience the best online
+          <h3 className=' text-xl font-semibold '>e-Shop</h3>
+          <p className=' mt-4'>Your one-step fot all your needs. Shop with use and experience the best online
             shopping experience.
           </p>
         </div>
-        <div>
-          <h4>Quick Links</h4>
-          <ul>
+        <div className='flex flex-col md:items-center'>
+          <h4 className='text-lg font-semibold'>Quick Links</h4>
+          <ul className='mt-4  space-y-2'>
             <li>
-            <Link>Home</Link>
+            <Link to="/" className=' hover:underline'>Home</Link>
             </li>
             <li>
-            <Link>Shop</Link>
+            <Link to="/shop" className=' hover:underline'>Shop</Link>
             </li>
             <li>
-            <Link>Contact</Link>
+            <Link to="/contact" className=' hover:underline'>Contact</Link>
             </li>
             <li>
-            <Link>About</Link>
+            <Link to="/about" className=' hover:underline'>About</Link>
             </li>
           </ul>
          
@@ -39,6 +39,10 @@ export const Footer = () => {
             <a href=""><FaGithub/></a>
             <a href=""><FaLinkedin/></a>
           </div>
+          <form>
+            <input type="email" placeholder='Enter your email' />
+            <button>Subscribe</button>
+          </form>
         </div>
       </div>
     </footer>

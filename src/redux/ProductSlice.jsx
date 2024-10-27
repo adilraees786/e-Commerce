2b646c4b78982327c3577aaaaabaf01fe4bc1b43
adilraees786 <adilraees911@gmail.com>
2024-10-27 18:50:21 +0500

    slice

--- src/redux/ProductSlice.jsx
+++ src/redux/ProductSlice.jsx
@@ -1,1 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const initialState ={
+    products: [],
+}
+
+const productSlice = createSlice({
+    name:"product",
+    initialState,
+    reducers:{
+        setProducts:(state,action)=>{
+            state.products = action.payload;
+        }
+    },
+})

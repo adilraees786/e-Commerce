20a035069eee6d3ea187e74210186d0f4e9bc73c
adilraees786 <adilraees911@gmail.com>
2024-10-27 20:38:07 +0500

    Addtailwindcss

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,20 +3,22 @@ import { FaStar } from 'react-icons/fa'
 
 export const ProductCard = ({product}) => {
   return (
-    <div>
-        <img src={product.image} alt='' />
-        <h3>{product.Name}</h3>
-        <p>${product.price}</p>
-        <div>
-            <FaStar></FaStar>
-            <FaStar></FaStar>
-            <FaStar></FaStar>
-            <FaStar></FaStar>
+    <div className=' bg-white p-4 shadow rounded relative border
+    transform transition-transform duration-300 hover:scale-105'>
+        <img src={product.image} alt={product.Name} className='w-full h-48 object-contain mb-4' />
+        <h3 className='text-lg font-semibold'>{product.Name}</h3>
+        <p className='text-gray-500'>${product.price}</p>
+        <div className='flex items-center mt-2 '>
+            <FaStar className='text-yellow-500'></FaStar>
+            <FaStar className='text-yellow-500'></FaStar>
+            <FaStar className='text-yellow-500'></FaStar>
+            <FaStar className='text-yellow-500'></FaStar>
         </div>
 
-        <div>
-            <span>+</span>
-            <span>Add to cart</span>
+        <div className=' absolute bottom-4 right-2 flex items-center justify-center w-8 h-8 bg-red-600
+        group text-white text-sm rounded-full hover:w-32 hover:bg-red-700 transition-all'>
+            <span className=' group-hover:hidden'>+</span>
+            <span className=' hidden group-hover:block'>Add to cart</span>
         </div>
 
     </div>

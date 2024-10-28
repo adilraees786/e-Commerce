f7125c4116f992dbe3297eed7e4fd3f5378e0894
adilraees786 <adilraees911@gmail.com>
2024-10-28 15:23:16 +0500

    footercopyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,33 +17,42 @@ export const Footer = () => {
           <h4 className='text-lg font-semibold'>Quick Links</h4>
           <ul className='mt-4  space-y-2'>
             <li>
-            <Link to="/" className=' hover:underline'>Home</Link>
+              <Link to="/" className=' hover:underline'>Home</Link>
             </li>
             <li>
-            <Link to="/shop" className=' hover:underline'>Shop</Link>
+              <Link to="/shop" className=' hover:underline'>Shop</Link>
             </li>
             <li>
-            <Link to="/contact" className=' hover:underline'>Contact</Link>
+              <Link to="/contact" className=' hover:underline'>Contact</Link>
             </li>
             <li>
-            <Link to="/about" className=' hover:underline'>About</Link>
+              <Link to="/about" className=' hover:underline'>About</Link>
             </li>
           </ul>
-         
+
         </div>
         <div>
           <h4 className=' text-lg font-semibold'>Follow Us</h4>
           <div className=' flex space-x-4 mt-4'>
-            <a href="" className='hover:text-gray-800'><FaFacebook/></a>
-            <a href="" className='hover:text-gray-800'><FaInstagram/></a>
-            <a href="" className='hover:text-gray-800'><FaGithub/></a>
-            <a href="" className='hover:text-gray-800'><FaLinkedin/></a>
+            <a href="" className='hover:text-gray-800'><FaFacebook /></a>
+            <a href="" className='hover:text-gray-800'><FaInstagram /></a>
+            <a href="" className='hover:text-gray-800'><FaGithub /></a>
+            <a href="" className='hover:text-gray-800'><FaLinkedin /></a>
           </div>
           <form className=' flex items-center justify-center mt-8'>
-            <input type="email" placeholder='Enter your email' 
-            className=' w-full p-2 rounded-l-lg bg-gray-800 border border-gray-600'/>
+            <input type="email" placeholder='Enter your email'
+              className=' w-full p-2 rounded-l-lg bg-gray-800 border border-gray-600' />
             <button className='bg-red-500 text-white px-4 py-2 rounded-r-lg  '>Subscribe</button>
           </form>
+        </div>
+      </div>
+      <div className=' mt-8 border-t border-gray-700 pt-4'>
+        <div className=' container mx-auto flex flex-col md:flex-row justify-between items-center'>
+          <p>&copy; 2024 e-shop All rights reserved</p>
+          <div className=' flex space-x-4 mt-4 md:mt-0'>
+            <a href="">Privacy Policy</a>
+            <a href="">Terms and Condition</a>
+          </div>
         </div>
       </div>
     </footer>

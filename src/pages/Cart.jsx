3d419970d2211ff25526f89c6553d245d4b30cfd
adilraees786 <adilraees911@gmail.com>
2024-10-29 19:06:21 +0500

    cartemptyimg

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,25 +1,56 @@
-// import React from 'react'
-// import { useSelector } from 'react-redux'
-// import EmptyCart from '../assets/Images/emptycart.png'
 
 
-// export const Cart = () => {
-//   const cart = useSelector(state => state.cart)
-
-//   return (
-//     <div>
-//       {cart.products.length > 0 ? 
-//       <div><div/>
-//       : <div>
-//         <img src={EmptyCart} alt="" />
-//         </div>}
-//     </div>
-
-//   )
-//   }
-    
+//   import React from 'react'
+//   import { useSelector } from 'react-redux'
+//   import EmptyCart from '../assets/Images/emptycart.png'
   
-  // export default Cart;
+//   const Cart = () => {
+//     const cart = useSelector(state => state.cart)
+  
+//     return (
+//       <div>
+//         {cart.products.length > 0 ? (
+//           <div>
+//             <h3>SHOPPING CART</h3>
+  
+//             <div>
+//               <div>
+//                 <div>
+//                   <p>PROUDUCTS</p>
+//                   <div>
+//                     <p>PRICE</p>
+//                     <p>QUANTITY</p>
+//                     <p>SUBTOTAL</p>
+//                     <p>REMOVE</p>
+                   
+//                   </div>
+//                   <div/>
+//                   <div>
+//                  {cart.products.map((product) =>(
+// <div>
+//   <div>
+//     <img src={product.image} alt="" />
+//   </div>
+//   <div/>
+
+//                  ))}
+//                   </div>
+//               </div>
+//               <div></div>
+        
+//             </div>
+//           </div>
+//         ) : (
+//           <div className="flex justify-center">
+//             <img src={EmptyCart} alt="Empty Cart" />
+//           </div>
+//         )}
+//       </div>
+//     )
+//   }
+  
+//   export default Cart;
+  
 
 import React from 'react'
 import { useSelector } from 'react-redux'
@@ -33,10 +64,33 @@ const Cart = () => {
       {cart.products.length > 0 ? (
         <div>
           <h3>SHOPPING CART</h3>
+
+          <div>
+            <div>
+              <p>PRODUCTS</p>
+              <div>
+                <p>PRICE</p>
+                <p>QUANTITY</p>
+                <p>SUBTOTAL</p>
+                <p>REMOVE</p>
+              </div>
+              
+              <div>
+                {cart.products.map((product) => (
+                  <div >
+                    <div>
+                      <img src={product.image} alt={product.name} />
+                    </div>
+                    <div></div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
       ) : (
-        <div className=' flex justify-center'>
-          <img src={EmptyCart} alt="" />
+        <div className="flex justify-center">
+          <img src={EmptyCart} alt="Empty Cart" />
         </div>
       )}
     </div>
@@ -44,4 +98,5 @@ const Cart = () => {
 }
 
 export default Cart;
+
 

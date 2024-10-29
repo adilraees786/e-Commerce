6bacc1c1a96b1294d31957089eb7d69e995b06ab
adilraees786 <adilraees911@gmail.com>
2024-10-29 18:49:42 +0500

    emptycartimg

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import  Navbar  from "./components/Navbar";
 import { Footer } from "./components/Footer";
 import  Home  from "./pages/Home";
 import { Shop } from "./pages/Shop";
-import { Cart } from "./pages/Cart";
+import  Cart  from "./pages/Cart";
 
 
 
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,13 +1,47 @@
+// import React from 'react'
+// import { useSelector } from 'react-redux'
+// import EmptyCart from '../assets/Images/emptycart.png'
+
+
+// export const Cart = () => {
+//   const cart = useSelector(state => state.cart)
+
+//   return (
+//     <div>
+//       {cart.products.length > 0 ? 
+//       <div><div/>
+//       : <div>
+//         <img src={EmptyCart} alt="" />
+//         </div>}
+//     </div>
+
+//   )
+//   }
+    
+  
+  // export default Cart;
+
 import React from 'react'
 import { useSelector } from 'react-redux'
+import EmptyCart from '../assets/Images/emptycart.png'
 
-
-export const Cart = () => {
-const cart = useSelector(state => state.cart)
+const Cart = () => {
+  const cart = useSelector(state => state.cart)
 
   return (
     <div>
-{cart.products}
+      {cart.products.length > 0 ? (
+        <div>
+          <h3>SHOPPING CART</h3>
+        </div>
+      ) : (
+        <div className=' flex justify-center'>
+          <img src={EmptyCart} alt="" />
+        </div>
+      )}
     </div>
   )
 }
+
+export default Cart;
+

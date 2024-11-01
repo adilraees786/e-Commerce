4b184573715e6787e61fb33a366181a5574cd8cc
adilraees786 <adilraees911@gmail.com>
2024-11-01 15:10:25 +0500

    increaseanddecrease

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -53,11 +53,15 @@ const Cart = () => {
                     <div className='flex space-x-12 items-center'>
                       <p>${product.price}</p>
                       <div className='flex items-center justify-center border'>
-                        <button className='text-xl font-bold px-1.5 border-r'>
+                        <button className='text-xl font-bold px-1.5 border-r'
+                        onClick={() => dispatch(decreaseQuantity(product.id))}>
                           -
                         </button>
                         <p className='text-xl px-2'>{product.quantity}</p>
-                        <button className='text-xl px-1 border-1'>+</button>
+                        <button className='text-xl px-1 border-1'
+                        onClick={() => dispatch(increaseQuantity(product.id))}> 
+                          +
+                        </button>
                       </div>
                       <p>${(product.quantity * product.price).toFixed(2)}</p>
                       <button className='text-red-500 hover:text-red-700'

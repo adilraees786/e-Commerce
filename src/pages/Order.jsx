61305dbfde908c146e87f538680ecc500a8c67b0
adilraees786 <adilraees911@gmail.com>
2024-11-07 13:25:43 +0500

    ordernavigate

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,6 +1,9 @@
+import { useNavigate } from 'react-router-dom';
 import React from 'react'
+import { Navigate } from 'react-router-dom';
 
 export const Order = ({ order }) => {
+    const navigate = useNavigate();
     return (
         <div className=' container mx-auto py-8 px-4 md:px-16 lg:px-24'>
             <h2 className=' text-2xl font-semibold mb-4'>Thank you for your order </h2>
@@ -30,7 +33,11 @@ export const Order = ({ order }) => {
                 </div>
                 <div className='mt-6'>
                     <button className=' ml-4 bg-green-500 text-white py-2 px-4 hover:bg-green-600'> Order tracking</button>
-                    <button className=' ml-4 bg-red-600 text-white py-2 px-4 hover:bg-red-800'> Continue Shopping</button>
+                    <button className=' ml-4 bg-red-600 text-white py-2 px-4 hover:bg-red-800' 
+                    onClick={() => navigate ('/')}>
+                         Continue Shopping
+                
+                    </button>
                 </div>
             </div>
         </div>

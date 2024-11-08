df85b42fa738cb4e3d614d929e5d95c0693ddf33
adilraees786 <adilraees911@gmail.com>
2024-11-08 16:35:34 +0500

    addlogindet

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
 export const Login = () => {
-  return (
-    <div>Login</div>
-  )
+    return (
+        <div>
+            <h2>Login</h2>
+            <form>
+                <div>
+                    <label htmlFor="">Email</label>
+                    <input type="email" />
+                </div>
+
+                <div>
+                    <label htmlFor="">Password</label>
+                    <input type="password" />
+                </div>
+<div>
+    <label htmlFor="">
+        <input type="checkbok" />
+        <span>Remember Me</span>
+    </label>
+</div>
+            
+
+            </form>
+        </div>
+    )
 }
 
 export default Login;

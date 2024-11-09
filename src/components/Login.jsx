80d9629296b64d26ca1354b7a3230f036ea9651e
adilraees786 <adilraees911@gmail.com>
2024-11-09 11:17:19 +0500

    addl0gincss

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,32 +3,34 @@ import React from 'react'
 export const Login = () => {
     return (
         <div>
-            <h2>Login</h2>
+            <h2 className=' text-2xl font-bold mb-4'>Login</h2>
             <form>
-                <div>
-                    <label htmlFor="">Email</label>
-                    <input type="email" />
+                <div className=' mb-4'>
+                    <label className="text-gray-700 block ">Email</label>
+                    <input type="email" className='w-full px-3 py-2 border'
+                    placeholder='Enter Email'/>
                 </div>
 
-                <div>
-                    <label htmlFor="">Password</label>
-                    <input type="password" />
+                <div className=' mb-4'>
+                    <label className="text-gray-700 block ">Password</label>
+                    <input type="password"  className='w-full px-3 py-2 border'
+                    placeholder='Enter Password' />
                 </div>
-                <div>
-                    <label htmlFor="">
-                        <input type="checkbok" />
-                        <span>Remember Me</span>
+                <div className=' mb-6 flex items-center justify-between'>
+                    <label className=" inline-flex items-center">
+                        <input type="checkbok" className=' form-checkbok' />
+                        <span className='ml-2 text-gray-700'>Remember Me</span>
                     </label>
-                    <a href="">Forgot Password?</a>
+                    <a href="" className='text-red-800'>Forgot Password?</a>
                 </div>
 
-                <div>
-                    <button>Login</button>
+                <div className=' mb-4'>
+                    <button type='submit' className=' w-full bg-red-600 text-white py-2'>Login</button>
                 </div>
             </form>
-            <div>
-                <span>Don't Have an Account?</span>
-                <button>Sign Up</button>
+            <div className=' text-center'>
+                <span className=' text-gray-700 '>Don't Have an Account?</span>
+                <button className=' text-red-800'>Sign Up</button>
             </div>
         </div>
     )

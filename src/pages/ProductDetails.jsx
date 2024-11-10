79bcbc85b02de9f869b8892e43da077629b230ac
adilraees786 <adilraees911@gmail.com>
2024-11-10 20:25:53 +0500

    addproductinfo

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,9 +1,38 @@
+import { useState } from 'react';
 import { useParams } from 'react-router-dom'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux';
 
-export default function ProductDetails(){
-    const {id} = useParams();
-  return (
-    <div>ProductDetails</div>
-  )
+
+export default function ProductDetails() {
+    const { id } = useParams();
+    const products = useSelector(state => state.product.products)
+    const [product, setProduct] = useState();
+
+    useEffect(() => {
+        const newProduct = products.find(
+            product => product.id === parseInt(id)
+        );
+        setProduct(newProduct);
+    };
+    [id, products]
+);
+
+    if (!product) {
+        return <div>Loading ...</div>
+    }
+    return (
+        <div className=' container mx-auto py-8 px-4 md:px-16 lg:px-24'>
+            <div className=' flex flex-col md:flex-row gap-x-16'>
+                {/* product image */}
+                <div className=' md:w-1/2 py-4 shadow-md md:px-8 h-96 flex justify-center'>
+                    <img src={product.image} alt={product.Name} className=" h-full" />
+
+                </div>
+
+{/* Product Information */}
+
+            </div>
+        </div>
+    )
 }

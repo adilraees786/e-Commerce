351acc78dd5142092128f974d95cd348a17065cc
adilraees786 <adilraees911@gmail.com>
2024-11-10 20:32:27 +0500

    addminitem

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -31,6 +31,18 @@ export default function ProductDetails() {
                 </div>
 
 {/* Product Information */}
+<div className=' md:w-1/2 p-4 shadow-md md:p-16 flex flex-col items-center gap-y-2'>
+<h2 className=' text-3xl font-semibold mb-2'>{product.Name}</h2>
+<p className=' text-xl font-semibold text-gray-800 mb-4'>
+    ${product.price}
+    </p>
+    <div className=' flex items-center mb-4 gap-x-2'>
+
+        <input id="quantity" type="number" min="1"
+         />
+
+    </div>
+</div>
 
             </div>
         </div>

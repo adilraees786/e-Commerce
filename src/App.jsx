74a3d21396b1f351882da17ea826b35ff0120f34
adilraees786 <adilraees911@gmail.com>
2024-11-10 20:03:01 +0500

    productdetails

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import  Checkout  from "./pages/Checkout";
 import { useState } from "react";
 import  Order  from "./pages/Order";
 import FilterData from "./pages/FilterData";
+import { ProductDetails } from "./pages/ProductDetails";
 
 
 
@@ -25,6 +26,7 @@ export default function App() {
         <Route path="/checkout" element={<Checkout setOrder={setOrder}/>}></Route>
         <Route path="/order-comfirmation" element={<Order order={order}/>}></Route>
         <Route path="/filter-data" element={<FilterData/>}></Route>
+        <Route path="/product/id" element={<ProductDetails/>}></Route>
 
        
       

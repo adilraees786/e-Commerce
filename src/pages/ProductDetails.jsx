ee414f9e622b6e404cb37ae698feaa11fde389db
adilraees786 <adilraees911@gmail.com>
2024-11-10 20:04:48 +0500

    addparamas

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,6 +1,8 @@
+import { useParams } from 'react-router-dom'
 import React from 'react'
 
-export const ProductDetails = () => {
+export default function ProductDetails(){
+    const {id} = useParams();
   return (
     <div>ProductDetails</div>
   )

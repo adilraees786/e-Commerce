69391b945ba56e9562f4e5b88d8a5315175d4a6f
adilraees786 <adilraees911@gmail.com>
2024-11-13 14:34:43 +0500

    homebold

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,7 @@ export default function Navbar() {
         <div className='bg-white shadow-md'> 
             <div className='container mx-auto px-4 lg:px-24 py-4 flex justify-between items-center'>
                 <div className='text-lg flex-1 mx-4'>
-                    <Link to="/" className='text-xl font-extrabold'>E-Shop</Link>
+                    <Link to="/" className='text-xl font-extrabold '>E-Shop</Link>
                 </div>
                 <div className='relative flex-1 mx-4'>
                     <form onSubmit={handleSearch}>
@@ -71,7 +71,7 @@ export default function Navbar() {
 
             {/* Start Menu */}
             <div className='flex items-center justify-center space-x-10 py-4 text-sm font-bold'>
-                <Link to="/" className='hover:underline'> 
+                <Link to="/" className='hover:underline font-extrabold'> 
                     Home
                 </Link>
                 <Link to="/shop" className='hover:underline'> 
